fa33ec15dd44268840a4aa68c240a50377731059
Pedro Santos <pholiveirasantos1091@gmail.com>
2022-11-19 19:35:57 -0300

    rota newsletter

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,33 +1,40 @@
 const post_index = async (req, res) => {
-  const [result] = await dbConnection.execute('SELECT * FROM posts');
-  res.json(result);
+  const [allPosts] = await dbConnection.execute('SELECT * FROM posts');
+  res.status(200).json(allPosts);
 };
 
 const post_details = async (req, res) => {
   const { id } = req.params;
-  const [[result]] = await dbConnection.execute(
+  const [[postDetail]] = await dbConnection.execute(
     'SELECT * FROM posts WHERE id = ?',
     [id],
   );
-  res.json(result);
+
+  if (!postDetail) res.status(404).json({});
+  else res.status(200).json(postDetail);
 };
 
 const post_create = async (req, res) => {
-  const { title, content } = req.body;
-  const result = await dbConnection.execute(
-    'INSERT INTO posts (title, content) VALUES (?,?)',
-    [title, content],
-  );
-
-  res.json(result);
+  try {
+    const { title, subheading, body } = req.body;
+    const [{ insertId }] = await dbConnection.execute(
+      'INSERT INTO posts (title, subheading, body) VALUES (?,?,?)',
+      [title, subheading, body],
+    );
+    res.status(200).json({ success: true, new_post_id: insertId });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const post_delete = async (req, res) => {
   const { id } = req.params;
-  const result = await dbConnection.execute('DELETE FROM posts WHERE id = ?', [
-    id,
-  ]);
-  res.json(result);
+  const [{ affectedRows }] = await dbConnection.execute(
+    'DELETE FROM posts WHERE id = ?',
+    [id],
+  );
+  if (affectedRows === 0) res.status(404).json({ error: 'Invalid id' });
+  else res.status(200).json({ success: true, deleted_post_id: id });
 };
 
 module.exports = { post_index, post_details, post_create, post_delete };
--- src/services/db.js
+++ src/services/db.js
@@ -1,17 +1,20 @@
 const mysql = require('mysql2/promise');
 
 async function connect() {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-  });
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    });
 
-  console.log('Banco de dados conectado.');
-
-  global.dbConnection = connection;
+    console.log('Banco de dados conectado.');
+    global.dbConnection = connection;
+  } catch (err) {
+    console.log('Erro ao conectar Banco de dados: ' + err);
+  }
 }
 
 connect();
