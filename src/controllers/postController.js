e3b6a78f8c5568002dd2a39fb3ce9f769dc99c71
Pedro Santos <pholiveirasantos1091@gmail.com>
2022-11-21 13:52:39 -0300

    update post - put /post/[id]

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,19 +1,28 @@
 const post_index = async (req, res) => {
-  const [allPosts] = await dbConnection.execute(
-    'SELECT id, title, subheading, created_at FROM post',
-  );
-  res.status(200).json(allPosts);
+  try {
+    const [allPosts] = await dbConnection.execute(
+      'SELECT id, title, subheading, created_at FROM post',
+    );
+    res.status(200).json(allPosts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const post_details = async (req, res) => {
   const { id } = req.params;
-  const [[postDetail]] = await dbConnection.execute(
-    'SELECT * FROM post WHERE id = ?',
-    [id],
-  );
 
-  if (!postDetail) res.status(404).json({});
-  else res.status(200).json(postDetail);
+  try {
+    const [[postDetail]] = await dbConnection.execute(
+      'SELECT * FROM post WHERE id = ?',
+      [id],
+    );
+
+    if (!postDetail) res.status(404).json({});
+    else res.status(200).json(postDetail);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const post_create = async (req, res) => {
@@ -25,18 +34,46 @@ const post_create = async (req, res) => {
     );
     res.status(200).json({ success: true, new_post_id: insertId });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
 const post_delete = async (req, res) => {
   const { id } = req.params;
-  const [{ affectedRows }] = await dbConnection.execute(
-    'DELETE FROM post WHERE id = ?',
-    [id],
-  );
-  if (affectedRows === 0) res.status(404).json({ error: 'Invalid id' });
-  else res.status(200).json({ success: true, deleted_post_id: id });
+  try {
+    const [{ affectedRows }] = await dbConnection.execute(
+      'DELETE FROM post WHERE id = ?',
+      [id],
+    );
+    if (affectedRows === 0)
+      res.status(404).json({ success: false, error: 'Post não encontrado' });
+    else res.status(200).json({ success: true, deleted_post_id: id });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
-module.exports = { post_index, post_details, post_create, post_delete };
+const post_update = async (req, res) => {
+  const { id } = req.params;
+  const { title, subheading, body } = req.body;
+  try {
+    const [{ affectedRows }] = await dbConnection.execute(
+      'UPDATE post SET title = ?, subheading = ?, body = ? WHERE id = ?',
+      [title, subheading, body, id],
+    );
+    if (affectedRows)
+      res.status(200).json({ success: true, updated_post_id: id });
+    else
+      res.status(404).json({ success: false, message: 'Post não encontrado' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  post_index,
+  post_details,
+  post_create,
+  post_delete,
+  post_update,
+};
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -5,12 +5,14 @@ const {
   post_details,
   post_create,
   post_delete,
+  post_update,
 } = require('../controllers/postController');
 const { verifyToken } = require('../controllers/authController');
 
 router.get('/', post_index);
 router.post('/', verifyToken, post_create);
 router.get('/:id', post_details);
+router.put('/:id', post_update);
 router.delete('/:id', verifyToken, post_delete);
 
 module.exports = router;
